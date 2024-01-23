1311610c14fb45f628e52958e0fd24b57247d1ad
vishal <vishalshenoy603@gmail.com>
2024-01-23 17:34:53 +0530

    commiting

--- JIRA/src/screens/Register Screen/Register.tsx
+++ JIRA/src/screens/Register Screen/Register.tsx
@@ -11,6 +11,8 @@ const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleRegister = () => {};
   return (
     <View style={styles.container}>
       <Text style={styles.text}>SIGN UP</Text>
